9187a4cc688b0c885abae37c46985fe3dd598f93
Atticus <atticus@macbook36gb.local>
2024-03-14 21:01:04 -0600

    fix(vite build): include source maps

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    minify: true,
+    cssMinify: true,
+  },
   plugins: [
     svgr(),
     react(),
